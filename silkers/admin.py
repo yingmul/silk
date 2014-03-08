a2f2736f365bdc34d593e917277f11b507fe982c
Audrey Liu <yingmul.at.work@gmail.com>
2014-03-09 00:19:09 +0800

    fix error from removing 'picture' from UserProfile

--- silkers/admin.py
+++ silkers/admin.py
@@ -3,6 +3,6 @@ from models import UserProfile
 
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['shoe_size', 'dress_size', 'picture']
+    list_display = ['shoe_size', 'dress_size']
 
 admin.site.register(UserProfile, UserProfileAdmin)
