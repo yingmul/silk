27bcb718b14fcd59d73ab0b0af815ffcd1ab32dd
Audrey Liu <yingmul.at.work@gmail.com>
2014-03-09 14:37:38 +0800

    set compress enabled to true for prod.py

--- settings/prod.py
+++ settings/prod.py
@@ -17,6 +17,7 @@ if environ.has_key('DATABASE_URL'):
         'PORT': url.port,
     }
 
+COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 COMPRESS_PRECOMPILERS = (('text/less', "lessc '{infile}' '{outfile}'"),)
 
